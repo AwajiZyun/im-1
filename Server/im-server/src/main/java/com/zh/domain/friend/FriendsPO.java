98c60e7da8d50c16087200467d86626bcb8d3283
zhangheng <zhangheng2683@gmail.com>
2018-12-24 18:24:37 +0800

    好友关系表实体

---
+++ Server/im-server/src/main/java/com/zh/domain/friend/FriendsPO.java
@@ -0,0 +1,16 @@
+package com.zh.domain.friend;
+
+import lombok.Data;
+
+/**
+ * 好友对象实体
+ */
+@Data
+public class FriendsPO {
+
+    private String id;
+
+    private String userCode;
+
+    private String friendCode;
+}
